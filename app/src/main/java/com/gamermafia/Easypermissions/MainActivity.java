f286625e61a61484091a01ea44e3de099e6f443f
GamerMafia <nik.tejani007@gmail.com>
2019-10-04 18:34:00 +0530

    update 1.1.1

--- app/src/main/java/com/gamermafia/Easypermissions/MainActivity.java
+++ app/src/main/java/com/gamermafia/Easypermissions/MainActivity.java
@@ -1,13 +1,13 @@
 package com.gamermafia.Easypermissions;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.gamerMafia.easypermission.EasyPermission;
 import com.gamerMafia.easypermission.listener.PermissionStatusListener;
